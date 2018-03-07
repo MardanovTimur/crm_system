68474467a734cb68c1bc104c8ac839b09c860d18
Mardanov Timur Rustemovich <timurmardanov97@gmail.com>
2018-03-07 14:10:04 +0300

    Initial Application created

---
+++ taxi/application/taxofon/main/index.js
@@ -0,0 +1,11 @@
+import React, {Component}  from 'react'
+import {Text} from 'react-native'
+
+export default class HelloWorld extends Component {
+    return() {
+        render (
+            <Text>Hello world!</Text>
+        )
+    }
+};
+
