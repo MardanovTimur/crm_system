eee396f833964a981456af0f764bc69e616faba8
Mardanov Timur Rustemovich <timurmardanov97@gmail.com>
2018-03-15 02:40:24 +0300

    Initial registration form

---
+++ taxi/application/taxofon/styles/buttons.js
@@ -0,0 +1,9 @@
+export const buttonStyle = (dict = {}) => {
+    return {
+        dict,
+        textColor : "#4db8ff",
+        backgroundColor : "#ffffff",
+        rippleColor: "#bfbfbf",
+
+    }
+};
