59f3f7221827691527e80e1165760306d66c0ee5
Ivan <iashevyakov@mail.ru>
2018-03-18 18:43:57 +0300

    auth v1

--- taxi/application/taxofon/components/initial.js
+++ taxi/application/taxofon/components/initial.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react'
 import {Button} from "react-native-material-design";
 import {Actions} from "react-native-router-flux";
 import {View} from "react-native";
 import {buttonStyle} from "../styles/buttons";
+import {Auth} from "./auth";
 
 export default class Initial extends Component {
     constructor(props) {
@@ -13,6 +13,7 @@ export default class Initial extends Component {
     render() {
         return (
             <View>
+                <Auth/>
                 <Button onPress={Actions.register} text={'Регистрация'} overrides={buttonStyle()}/>
             </View>
         )
