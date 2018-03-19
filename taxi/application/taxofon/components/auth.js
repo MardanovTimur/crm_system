be748f159e152b35b22bb00e4b4b5861698eda94
Ivan <iashevyakov@mail.ru>
2018-03-19 23:59:53 +0300

    auth v2

--- taxi/application/taxofon/components/auth.js
+++ taxi/application/taxofon/components/auth.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {Button} from "react-native-material-design";
-import {TextInput, View} from "react-native";
+import {TextInput, View, StyleSheet, Text, TouchableOpacity} from "react-native";
 import {buttonStyle} from "../styles/buttons";
 import PhoneInput from "react-native-phone-input";
 import {countriesList, phonePlaceHolder, phoneStyle} from "../styles/text";
@@ -15,27 +15,53 @@ export class Auth extends Component {
         }
     }
 
-    render() {
+    render () {
         return (
-            <View style={{
-                flex : 1,
-                margin: 20,
-            }}>
-                <View style={{flex: 1, marginTop:100}}>
-                    <PhoneInput
-                        ref={(ref) => { this.phone = ref; }} initialCountry={'ru'} textStyle={phoneStyle()}
-                        textProps={phonePlaceHolder()}
-                    />
-                    <PasswordInputText
-                        value={this.state.password}
-                        onChangeText={ (password) => this.setState({ password })
-                        }
-                    />
-                    <View style={{margin:7}} />
-                    <Button text={'Войти'} overrides={buttonStyle()}/>
-
-                </View>
+            <View style = {styles.authform}>
+                <Text style = {styles.header}>
+                    Авторизация
+                </Text>
+                <TextInput style = {styles.textinput} placeholder="Ваш телефон" underlineColorAndroid ={'transparent'}/>
+                <TextInput style = {styles.textinput} placeholder="Пароль" secureTextEntry = {true} underlineColorAndroid ={'transparent'}/>
+                <TouchableOpacity style = {styles.button}>
+                    <Text style = {styles.btntext}>Войти</Text>
+                </TouchableOpacity>
             </View>
-        );
+        )
     }
+
 }
+
+const styles = StyleSheet.create({
+   authform : {
+        alignSelf: 'stretch',
+   },
+   header: {
+        fontSize: 24,
+        color: '#fff',
+        paddingLeft: 45,
+        paddingBottom: 5,
+        marginBottom: 35,
+   },
+    textinput: {
+       alignSelf: 'stretch',
+        height: 40,
+        marginBottom: 30,
+        color: '#fff',
+        borderBottomColor: '#f8f8f8',
+        borderBottomWidth: 1,
+    },
+    button : {
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: '#59cbbd',
+        marginTop: 30,
+    },
+    btntext: {
+        color: '#fff',
+        fontWeight: 'bold',
+    }
+
+});
+
