7fd75d6c7d7859bbe94fa9495566ab047244d742
Mardanov Timur Rustemovich <timurmardanov97@gmail.com>
2018-03-24 00:53:02 +0300

    Expo system

---
+++ taxi/application/taxofon/components/initialMap.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import Map from "./map";
+
+export default class MainPage extends Component {
+
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <Map/>
+        );
+    }
+}
+
--- taxi/application/taxofon/components/map.js
+++ taxi/application/taxofon/components/map.js
@@ -3,23 +3,41 @@ import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
 
 
-
 export default class Map extends Component {
     constructor(props) {
         super(props);
 
     }
 
-    onRegionChange(region) {
-        this.setState({ region });
-    }
-
     render() {
         return (
-            <MapView
-                region={this.state.region}
-                onRegionChange={this.onRegionChange}
-            />
+            <View style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: 55.797520,
+                        longitude: 49.114933,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                >
+                </MapView>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
+    }
+});
